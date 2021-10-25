92bf6ffdb490d962e9e53025538ce9ecc2b63ca6
test <zamsergey@gmail.com>
2021-10-26 00:32:52 +0300

    Доработка модуля карточки

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import {generateData} from './data.js';
-import {getPlaceHtml} from './sameBlocks.js';
+import {showCard} from './card.js';
 
-const placeContainer = document.querySelector('#map-canvas');
+
 const testPlaces = generateData(1);
-const popupBlockTemplate = document.querySelector('#card').content.querySelector('.popup');
 
-const testRent = getPlaceHtml(popupBlockTemplate,testPlaces);
-placeContainer.appendChild(testRent);
+
+showCard(testPlaces[0]);
+
