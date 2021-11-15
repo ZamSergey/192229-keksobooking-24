6eb35d59cce2e2fd587779130d14af53177bbe25
test <zamsergey@gmail.com>
2021-11-15 22:42:48 +0300

    Финальные правки

--- js/main.js
+++ js/main.js
@@ -1,39 +1,35 @@
 import {activateMap,drawMapData,updateMap,initLat,initLng} from './map.js';
 import {loadData} from './load.js';
-import {enabledForm,setAddressCoordinate,setEvant} from './form.js';
-import {filterData} from './filter.js';
+import {enabledForm,setAddressCoordinate} from './form.js';
+import {filterData,setFilterEventListener} from './filter.js';
 import {debounce} from './utils.js';
+import {showAlert} from './message.js';
 
 const DATA_LIMIT = 10;
 let dataFromServer = [];
 
-const getDataOnSuccess = (data) => {
-  dataFromServer = data;
-  drawMapData(data.slice().slice(0,DATA_LIMIT));
-};
-
-const getDataOnError = (error) => {
-  throw new Error(`При загрузке данных произошла ошибка ${error}`);
-};
-
 const getData = () => dataFromServer;
 
-const activatePage = () => {
+const changeMapFilter = () => {
+  const arrData = filterData(getData());
+  updateMap(arrData.slice(0,DATA_LIMIT));
+};
+
+const getDataOnSuccess = (data) => {
+  dataFromServer = data;
+  drawMapData(filterData(dataFromServer).slice(0,DATA_LIMIT));
   enabledForm();
+};
+
+const getDataOnError = (error) =>  showAlert(`При загрузке данных произошла ошибка ${error}`);
+
+const activatePage = () => {
   setAddressCoordinate(initLat, initLng);
   loadData(getDataOnSuccess,getDataOnError);
 };
 
+
 activateMap(activatePage);
+setFilterEventListener(debounce(changeMapFilter));
 
-const changeMapFilter = () => {
-  const arrData = filterData(getData());
-  if(arrData.length > DATA_LIMIT) {
-    updateMap(arrData.slice(0,DATA_LIMIT));
-  }
-  else {
-    updateMap(arrData);
-  }
-};
-
-setEvant(debounce(changeMapFilter));
+export {changeMapFilter};
